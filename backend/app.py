7ba1eb8243bb651baecac5c537776f9d17c7ebb5
Ayda Elsanosi <ghaidaasan97@gmail.com>
2025-12-06 20:17:33 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -46,5 +46,6 @@ def health():
     return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
 
